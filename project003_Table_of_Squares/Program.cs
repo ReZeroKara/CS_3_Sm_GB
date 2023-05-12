370f00c00c9da7436ed1f1a1503c3d0e53ccaa06
ReZero <ghostofpast@icloud.com>
2023-05-12 18:59:37 +0300

    add pj3

---
+++ project003_Table_of_Squares/Program.cs
@@ -0,0 +1,52 @@
+﻿//Программа, которая принимает на вход число N и выдаёт таблицу квадратов чисел от 1 до N.
+
+Main();
+
+void Main()
+{
+    bool isWorking = true;
+
+    while(isWorking)
+    {
+        Console.Write("Input task: ");
+        string task = Console.ReadLine();
+
+        switch(task)
+        {
+            case "t1": Task1(); break;
+            case "exit": isWorking = false; break;
+            default:
+            break;
+        }
+    }
+}
+
+void Task1()
+{
+    int number = ReadInt();
+    int nul = 1;
+
+    Console.Write("[");
+
+    while (nul <= number)
+    {
+        double result = Math.Pow((nul), 2);
+        Console.Write(result + ", ");
+        nul++;
+    }
+
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int ReadInt()
+{
+    Console.Write("Input positive number: ");
+    int N;
+    
+    while (!int.TryParse(Console.ReadLine(), out N))
+    {
+        Console.WriteLine("It's not a number!");
+    }
+    return N;
+}
